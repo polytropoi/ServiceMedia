61404614f4927ccfa00cf2d87557623d9e02e8d0
Jim Cherry <polytropoi@gmail.com>
2022-08-25 16:31:32 -0500

    cleanup

--- routes/oculus_routes.js
+++ routes/oculus_routes.js
@@ -4,47 +4,47 @@ const axios =  require("axios");
 
 oculus_router.post("/:app/:action", function (req, res) {
     
-        if (req.params.app.toString().toLowerCase() == "cowbots_rift") {
-            console.log("oculus request for cowbots " + JSON.stringify(req.body));
-            if (req.params.action.toString().toLowerCase() == "validate") {
-                let data = {};
-                data.access_token = process.env.COWBOTS_OCULUS_RIFT_TOKEN;
-                data.nonce = req.body.nonce;
-                data.user_id = req.body.oID;
-                console.log(JSON.stringify(data));
-                axios.post("https://graph.oculus.com/user_nonce_validate/", data) 
-                .then((response) => {
+    if (req.params.app.toString().toLowerCase() == "cb_rift") {
+        console.log("oculus request for cowbots " + JSON.stringify(req.body));
+        if (req.params.action.toString().toLowerCase() == "validate") {
+            let data = {};
+            data.access_token = process.env.CB_OCULUS_RIFT_TOKEN;
+            data.nonce = req.body.nonce;
+            data.user_id = req.body.oID;
+            console.log(JSON.stringify(data));
+            axios.post("https://graph.oculus.com/user_nonce_validate/", data) 
+            .then((response) => {
                 // console.log("oculus api validaaqtion response: " + JSON.stringify(response.data));
                 res.send("nonce validation response: " + JSON.stringify(response.data));
-                })
-                .catch(function (error) {
-                    res.send(error);
-                })
-            } else {
-                res.send("dunno...no action");
-            }
-        } else if ((req.params.app.toString().toLowerCase() == "cowbots_quest")) {
-            if (req.params.action.toString().toLowerCase() == "validate") {
-                let data = {};
-                data.access_token = process.env.COWBOTS_OCULUS_QUEST_TOKEN;
-                data.nonce = req.body.nonce;
-                data.user_id = req.body.oID;
-                console.log(JSON.stringify(data));
-                axios.post("https://graph.oculus.com/user_nonce_validate/", data)
-                .then((response) => {
-                // console.log("oculus api validaaqtion response: " + JSON.stringify(response.data));
-                res.send("nonce validation response: " + JSON.stringify(response.data));
-                })
-                .catch(function (error) {
-                    res.send(error);
-                })
-            } else {
-                res.send("dunno...no action");
-            }
+            })
+            .catch(function (error) {
+                res.send(error);
+            })
         } else {
-            res.send("for who/what?");
+            res.send("dunno...no action");
         }
-    });
+    } else if ((req.params.app.toString().toLowerCase() == "cb_quest")) {
+        if (req.params.action.toString().toLowerCase() == "validate") {
+            let data = {};
+            data.access_token = process.env.CB_OCULUS_QUEST_TOKEN;
+            data.nonce = req.body.nonce;
+            data.user_id = req.body.oID;
+            console.log(JSON.stringify(data));
+            axios.post("https://graph.oculus.com/user_nonce_validate/", data)
+            .then((response) => {
+                // console.log("oculus api validaaqtion response: " + JSON.stringify(response.data));
+                res.send("nonce validation response: " + JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                res.send(error);
+            })
+        } else {
+            res.send("dunno...no action");
+        }
+    } else {
+        res.send("for who/what?");
+    }
+});
 
     oculus_router.get("/test", function (req, res) {
         res.send("OK!");
