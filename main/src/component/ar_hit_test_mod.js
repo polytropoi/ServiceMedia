945d660316b8a8b2b5ff9b66b9b8ffb2cc0162f3
polytropoi <polytropoi@gmail.com>
2024-11-27 10:00:36 -0600

    worldToLocal fix?

--- main/src/component/ar_hit_test_mod.js
+++ main/src/component/ar_hit_test_mod.js
@@ -29,24 +29,14 @@ AFRAME.registerComponent('ar_hit_test_mod', {
         console.log("ar-mode");
         // let arTargetEls = document.querySelectorAll(".ar_target");
         let arTarget = document.getElementById("ar_target");
-        if (arTarget) {
-        // for (let i = 0; i < arTargetEls.length; i++) {     
-        //       arTarget.appendChild(arTargetEls[i]); //????
-        //   }
-          
-          document.querySelectorAll('.arTarget').forEach(function(el) {
-              // arTarget.appendChild(el);
-              // el.object3D.updateMatrixWorld();
-              // let targetEl = {};
-              targetEl.position = el.getAttribute("position");
-              targetEl.id = el.id;
-              // el.setAttribute("scale", "0.1 0.1 0.1");
-              arTargetData.push(targetEl);
+        if (arTarget) {       
+            document.querySelectorAll('.arTarget').forEach(function(el) { //els with "ar target" tag
+            targetEl.position = el.getAttribute("position");
+            targetEl.id = el.id;
+            arTargetData.push(targetEl);
           });
         }
-        if (arTargetData.length) {
-          targetEl.object3D.visible = false;
-        }
+
         session = self.el.sceneEl.renderer.xr.getSession();
   
         self.originalPosition = targetEl.object3D.position.clone();
@@ -66,6 +56,9 @@ AFRAME.registerComponent('ar_hit_test_mod', {
           }
 
           // console.log("hit test position selected " + JSON.stringify(position));
+          if (arTargetData.length) {
+            targetEl.object3D.visible = false;
+          }
           for (let i = 0; i < arTargetData.length; i++) {
             // el.object3D.worldToLocal(localPosition);
             // console.log("tryna set id " + arTargetData[i].id + " position " + position);
@@ -76,7 +69,7 @@ AFRAME.registerComponent('ar_hit_test_mod', {
               let arTargetPosition = new THREE.Vector3(); //get position data from components if found
               let localMarkerComponent = arTargetEl.components.local_marker;
               if (localMarkerComponent) {
-                arTargetPosition = targetEl.worldToLocal(parseFloat(localMarkerComponent.data.xpos, parseFloat(localMarkerComponent.data.ypos), parseFloat(localMarkerComponent.data.zpos)));
+                arTargetPosition = targetEl.object3D.worldToLocal(parseFloat(localMarkerComponent.data.xpos, parseFloat(localMarkerComponent.data.ypos), parseFloat(localMarkerComponent.data.zpos)));
                 console.log("localmarker arTargetPosition " + JSON.stringify(arTargetPosition));
 
                 arTargetEl.setAttribute('position', arTargetPosition);
@@ -86,7 +79,7 @@ AFRAME.registerComponent('ar_hit_test_mod', {
               } else {
                 let cloudMarkerComponent = arTargetEl.components.cloud_marker;
                 if (cloudMarkerComponent) {
-                  arTargetPosition = targetEl.worldToLocal(parseFloat(cloudMarkerComponent.data.xpos, parseFloat(cloudMarkerComponent.data.ypos), parseFloat(cloudMarkerComponent.data.zpos)));
+                  arTargetPosition = targetEl.object3D.worldToLocal(parseFloat(cloudMarkerComponent.data.xpos, parseFloat(cloudMarkerComponent.data.ypos), parseFloat(cloudMarkerComponent.data.zpos)));
                   console.log("cloudmarker arTargetPosition " + JSON.stringify(arTargetPosition));
                   arTargetEl.setAttribute('position', arTargetPosition);
                   // arTargetEl.setAttribute('scale', {x: parseFloat(cloudMarkerComponent.data.xscale) * 0.2, 
@@ -95,7 +88,7 @@ AFRAME.registerComponent('ar_hit_test_mod', {
                 } else {
                   let modModelComponent = arTargetEl.components.mod_model;
                   if (modModelComponent) {
-                    arTargetPosition = targetEl.worldToLocal(parseFloat(modModelComponent.data.xpos, parseFloat(modModelComponent.data.ypos), parseFloat(modModelComponent.data.zpos)));
+                    arTargetPosition = targetEl.object3D.worldToLocal(parseFloat(modModelComponent.data.xpos, parseFloat(modModelComponent.data.ypos), parseFloat(modModelComponent.data.zpos)));
                     console.log("modmodel arTargetPosition " + JSON.stringify(arTargetPosition));
                     arTargetEl.setAttribute('position', position);
                     // arTargetEl.setAttribute('scale', {x: parseFloat(modModelComponent.data.xscale) * 0.2, 
@@ -104,7 +97,7 @@ AFRAME.registerComponent('ar_hit_test_mod', {
                   } else {
                       let modObjectComponent = arTargetEl.components.mod_object;
                       if (modObjectComponent) {
-                        arTargetPosition = targetEl.worldToLocal(parseFloat(modObjectComponent.data.xpos, parseFloat(modObjectComponent.data.ypos), parseFloat(modObjectComponent.data.zpos)));
+                        arTargetPosition = targetEl.object3D.worldToLocal(parseFloat(modObjectComponent.data.xpos, parseFloat(modObjectComponent.data.ypos), parseFloat(modObjectComponent.data.zpos)));
                         console.log("modobject arTargetPosition " + JSON.stringify(arTargetPosition));
                         arTargetEl.setAttribute('position', position);
                         // arTargetEl.setAttribute('scale', {x: parseFloat(modObjectComponent.data.xscale) * 0.2, 
