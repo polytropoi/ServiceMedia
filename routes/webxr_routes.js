6bc21e97bdbfbee78283fb86a228fb5a86efde76
polytropoi <polytropoi@gmail.com>
2024-12-18 15:18:52 -0600

    hand mods2

--- routes/webxr_routes.js
+++ routes/webxr_routes.js
@@ -1728,9 +1728,9 @@ webxr_router.get('/:_id', function (req, res) {
                                             }
                                             if (sceneResponse.sceneTags != null && (sceneResponse.sceneTags.includes('hand controls') || sceneResponse.sceneTags.includes('hand controls'))) {
                                                 lookcontrols = "look-controls"; // because magicwinders enabled by default
-                                                handEntities = "<a-entity id=\x22left-hand\x22 hand-controls=\x22hand: left\x22 oculus-touch-controls=\x22hand: left;\x22></a-entity>" +
+                                                handEntities = "<a-entity id=\x22left-hand\x22 oculus-touch-controls=\x22hand: left;\x22 hand-tracking-controls=\x22hand: left;\x22></a-entity>" +
                                                                 "<a-sphere color=\x22blue\x22 opacity=\x220.1\x22 radius=\x220.06\x22 "+ammoHands+"></a-sphere></a-entity>" +
-                                                                "<a-entity id=\x22right-hand\x22 hand-controls=\x22hand: right\x22 oculus-touch-controls=\x22hand: right;\x22>" +
+                                                                "<a-entity id=\x22right-hand\x22 hand-controls=\x22hand: right\x22 oculus-touch-controls=\x22hand: right;\x22 hand-tracking-controls=\x22hand: right;\x22>" +
                                                                 "<a-sphere color=\x22orange\x22 opacity=\x220.1\x22 radius=\x220.06\x22 "+ammoHands+"></a-sphere>"+
                                                                 "<a-entity id=\x22rightHandEquip\x22 controller_ball_blaster rotation=\x22-80 0 0\x22 position=\x22-0.02 0 -0.01\x22></a-entity>" +
                                                                 "</a-entity>";
