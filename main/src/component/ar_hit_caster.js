cfa7ca18887a0f02d77eae47faa6e74675930b21
polytropoi <polytropoi@gmail.com>
2024-12-19 16:15:23 -0600

    optionalFeatures

--- main/src/component/ar_hit_caster.js
+++ main/src/component/ar_hit_caster.js
@@ -56,7 +56,7 @@ AFRAME.registerComponent('ar_hit_caster', {
         if (!self.el.sceneEl.is('ar-mode')) { 
           return; 
         }
-        targetEl.setAttribute('visible', false);
+        // targetEl.setAttribute('visible', false);
 
         session = self.el.sceneEl.renderer.xr.getSession();
         self.messageEl.textContent = "entered AR mode";
@@ -109,9 +109,9 @@ AFRAME.registerComponent('ar_hit_caster', {
           var distance = position.distanceTo(zeroPos);
 
           var scaleMod = .02 + (distance * 0.2);
-          if ((position.x != 0) && (position.y != 0) && (position.z != 0)) {
-            targetEl.setAttribute('visible', true);
-          }
+          // if ((position.x != 0) && (position.y != 0) && (position.z != 0)) {
+          //   targetEl.setAttribute('visible', true);
+          // }
           if (!self.lockTargets) {
             console.log("tryna set position " + JSON.stringify(position) + " distance " + JSON.stringify(distance));
             self.messageEl.textContent = "surface found at position " + JSON.stringify(position);
